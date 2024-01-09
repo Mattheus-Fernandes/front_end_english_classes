bc27530c95a43982158e899d7e6cd867b7e8a793
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-09 10:24:07 -0300

    create header

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+   <nav className="d-flex justify-content-between align-items-center bg-dark p-2 mb-3">
+      <NavLink to="/" className="text-decoration-none text-white">
+        <span className="fs-2">Class scheduling</span>
+      </NavLink>
+      <ul className={styles.ul}>
+          <li>
+            <NavLink to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/search/class">
+              Search class
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/edit/class">
+              Change
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/register/class">
+              Register
+            </NavLink>
+          </li>
+          
+      </ul>
+   </nav>     
+  )
+}
+
+export default Header
